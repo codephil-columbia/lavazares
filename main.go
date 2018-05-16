103da4efdd38b1e63a2082056baeb0a9d37cc761
cesaribarra10 <cfi2103@columbia.edu>
2018-05-16 15:05:31 -0700

    deleted old app folder, changed db connection string

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -22,15 +21,6 @@ const (
 )
 
 func main() {
-
-	user := flag.String("user", "codephil", "database user")
-	dbname := flag.String("dbname", "lavazaresDB", "dbname")
-	password := flag.String("password", "password", "dbpassword")
-	port := flag.String("port", "5432", "dbport")
-	host := flag.String("host", "localhost", "dbhost")
-	ssl := flag.String("ssl", "disable", "ssl mode")
-
-	connStr := fmt.Sprintf("user=%s dbname=%s, password=%s, port-%s, host=%s, sslmode=%s", *user, *dbname, *password, *port, *host, *ssl)
 
 	if err := models.InitDB(connStr); err != nil {
 		fmt.Println(err)
